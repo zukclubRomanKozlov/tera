ea85cafdc1f75e2ca83f8b701a21e33c2fcbd5ea
katerinazhebova <78918018+katerinazhebova@users.noreply.github.com>
2021-02-16 21:55:32 +0300

    Add files via upload
    Zhebova ПИ18-4

--- like_button.js
+++ like_button.js
@@ -1,34 +1,49 @@
 'use strict';
 
+
+
 const e = React.createElement;
 
+
+
 class LikeButton extends React.Component {
-constructor(props) {
-super(props);
-this.state = { liked: false };
+
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ' + this.props.commentID;
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+
+  }
+
 }
 
-render() {
-if (this.state.liked) {
-return 'You liked comment number ' + this.props.commentID;
-}
 
-return e(
-'button',
-{ onClick: () => this.setState({ liked: true }) },
-'Like'
-);
-}
-}
 
-// const domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(e(LikeButton), domContainer);
 
 document.querySelectorAll('.like_button_container')
-.forEach(domContainer => {
-const commentID = parseInt(domContainer.dataset.commentid, 10);
-ReactDOM.render(
-e(LikeButton, { commentID: commentID}),
-domContainer
-);
-});
+
+  .forEach(domContainer => {
+
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+
+    ReactDOM.render(
+
+      e(LikeButton, { commentID: commentID }),
+
+      domContainer
+
+    );
+  });
